739f9c8c2ec71c05eda73678ffa42e1bc8d5b6f4
brian <bybyby@gmail.com>
2020-06-22 23:54:00 -0700

    Stage 3/5: Bit-level error emulator.  Corrupt a random bit in each byte.

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,54 +1,38 @@
 package correcter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        String TEST = "11122233344455566677788899";
-//        Scanner sc = new Scanner(TEST);
 
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder(sc.nextLine());
-        StringBuilder sbTripled = new StringBuilder();
-        StringBuilder sbDecoded = new StringBuilder();
+        String fileSend = "send.txt";
+        String fileReceived = "received.txt";
+
+        File file = new File("send.txt");
+        byte[] bytesArray = new byte[(int) file.length()];
+
+        // Open txt file, then read in as byte stream
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(bytesArray);
+        } catch (Exception e) {
+            System.out.println("Exception FileInputStream: " + e);
+        }
+
         Random random = new Random();
-        String charErrors = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
-        int len = charErrors.length();
-
-        // Output original input
-        System.out.println(sb.toString());
-
-        // Output triple characters
-        for (int i = 0; i < sb.length(); i++) {
-            for (int j = 0; j < 3; j++) {
-                sbTripled.append(sb.charAt(i));
-            }
+        // Change byte
+        for (int i = 0; i < bytesArray.length; i++) {
+            bytesArray[i] ^= 1 << random.nextInt(8);
         }
-        System.out.println(sbTripled);
 
-        // Create errors in every triple
-        for (int i = 0; i < sbTripled.length(); i += 3) {
-            int randPlace = random.nextInt(3);
-            if (randPlace < sbTripled.length()) {
-                sbTripled.replace(i + randPlace, i + randPlace + 1,
-                        String.valueOf(charErrors.charAt(random.nextInt(len - 1))));
-            }
+        // Read byte stream, then write out to file
+        try (FileOutputStream fos = new FileOutputStream(fileReceived);) {
+            fos.write(bytesArray);
+        } catch (Exception e) {
+            System.out.println("Exception FileOutputStream: " + e);
         }
-        System.out.println(sbTripled.toString());
-
-        // Decode back correct message
-        for (int i = 0; i < sbTripled.length(); i += 3) {
-            if (sbTripled.charAt(i) == sbTripled.charAt(i + 1)) {
-                sbDecoded.append(sbTripled.charAt(i));
-            } else if (sbTripled.charAt(i) == sbTripled.charAt(i + 2)) {
-                sbDecoded.append(sbTripled.charAt(i));
-            } else if (sbTripled.charAt(i + 1) == sbTripled.charAt(i + 2)) {
-                sbDecoded.append(sbTripled.charAt(i + 1));
-            }
-        }
-        System.out.println(sbDecoded.toString());
-
     }
 }
 
